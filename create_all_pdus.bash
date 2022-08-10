125e9fd6bb61872ac561c1e615cd1410cbd33ea2
Takashi Mori <kanetugu2015@gmail.com>
2022-08-11 08:59:21 +0900

    add create tools for can

---
+++ create_all_pdus.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function create_dependent_pdus()
+{
+	COUNT=`wc -l dep_types.txt | awk '{print $1}'`
+	if [ ${COUNT} -eq 0 ]
+	then
+		return
+	fi
+	for dep_msg in `cat dep_types.txt`
+	do
+		bash create_pdu_ctypes.bash ./config/search_path.txt ${dep_msg}
+	done
+}
+
+for msg in `cat ./config/ros_msgs.txt`
+do
+	bash create_pdu_ctypes.bash ./config/search_path.txt ${msg}
+	create_dependent_pdus
+done
