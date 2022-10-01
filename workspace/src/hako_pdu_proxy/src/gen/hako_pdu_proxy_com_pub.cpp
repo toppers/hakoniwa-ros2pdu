f72034e2b078f55491f8a56f85fac10c0e16ad5f
Takashi Mori <kanetugu2015@gmail.com>
2022-10-01 16:14:42 +0900

    gen codes are refactored

--- workspace/src/hako_pdu_proxy/src/gen/hako_pdu_proxy_com_pub.cpp
+++ workspace/src/hako_pdu_proxy/src/gen/hako_pdu_proxy_com_pub.cpp
@@ -7,15 +7,14 @@
 static std::shared_ptr<rclcpp::Node> my_node;
 
 //TODO repeat
-static std::shared_ptr<rclcpp::Publisher<can_msgs::msg::HakoCan>> publisher_xxx;
-
+DECLARE_PUBLISHER(can_msgs::msg::HakoCan, sample_topic_name);
 
 void hako_pdu_proxy_com_pub_init(std::shared_ptr<rclcpp::Node> node)
 {
     my_node = node;
 
     //TODO repeat..
-    publisher_xxx = my_node->create_publisher<can_msgs::msg::HakoCan>("topic_name", 1);
+    CREATE_PUBLISHER(can_msgs::msg::HakoCan, sample_topic_name);
 
     return;
 }
@@ -23,11 +22,7 @@ void hako_pdu_proxy_com_pub_init(std::shared_ptr<rclcpp::Node> node)
 void hako_pdu_proxy_com_publish(void)
 {
     //TODO repeat
-    auto can_msg = can_msgs::msg::HakoCan();
-    Hako_HakoCan hako_can_msg;
-    if (hako_pdu_proxy_rx_data(0, hako_can_msg)) {
-        hako_convert_pdu2ros_HakoCan(hako_can_msg, can_msg);
-        publisher_xxx->publish(can_msg);
-    }
+    PUBLISH_PDU_TOPIC(can_msgs, HakoCan, 0, sample_topic_name);
+
     return;
 }
--- workspace/src/hako_pdu_proxy/src/hako_pdu_proxy_com.hpp
+++ workspace/src/hako_pdu_proxy/src/hako_pdu_proxy_com.hpp
@@ -17,5 +17,28 @@ extern void hako_pdu_proxy_com_publish(void);
 /*
  * internal apis
  */
+#define DECLARE_PUBLISHER(type, topic_name) \
+    static std::shared_ptr<rclcpp::Publisher<type>> publisher_ ##topic_name
+#define PUBLISHER(topic_name) publisher_ ##topic_name
+#define CREATE_PUBLISHER(type, topic_name) \
+do {    \
+    PUBLISHER(topic_name) = my_node->create_publisher<type>(#topic_name, 1);    \
+} while (0)
+
+#define PUBLISH_TOPIC(topic_name, msg)   \
+do {    \
+    PUBLISHER(topic_name)->publish(msg);    \
+} while (0)
+
+#define PUBLISH_PDU_TOPIC(pkg, ros_type, channel_id, topic_name) \
+do {    \
+    auto ros_msg = pkg::msg::ros_type();  \
+    Hako_ ##ros_type pdu_msg;   \
+    if (hako_pdu_proxy_rx_data((channel_id), pdu_msg)) {    \
+        hako_convert_pdu2ros_ ##ros_type (pdu_msg, ros_msg); \
+        PUBLISH_TOPIC(topic_name, ros_msg);   \
+    }   \
+} while (0)
+
 
 #endif /* _CAN_PROXY_COM_HPP_ */
