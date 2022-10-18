f36bd51bad1f4df3c6f70f3da0f5be99c0ed745f
Takashi Mori <kanetugu2015@gmail.com>
2022-10-18 15:30:22 +0900

    add selector

--- utils/binary/offset_parser.py
+++ utils/binary/offset_parser.py
@@ -2,10 +2,19 @@
 # -*- coding: utf-8 -*-
 
 
+from select import select
+
+
 def parse_offset(filepath):
     with open(filepath) as f:
         lines = f.readlines()
         return lines
+
+def select_by_name(lines, name):
+    for line in lines:
+        if member_name(line) == name:
+            return line
+    return None
 
 def member_name(data):
     return data.split(":")[2]
@@ -26,4 +35,3 @@ def array_size(data):
 
 def member_off(data):
     return int(data.split(":")[4])
-
