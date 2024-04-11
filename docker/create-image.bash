d6dc081cc6996c6dc7c5bfb3e8959e995f1949b2
Takashi Mori <kanetugu2015@gmail.com>
2024-04-11 12:23:53 +0900

    bug fixed

--- docker/create-image.bash
+++ docker/create-image.bash
@@ -4,5 +4,21 @@ IMAGE_NAME=`cat docker/image_name.txt`
 IMAGE_TAG=`cat appendix/latest_version.txt`
 DOCKER_IMAGE=${IMAGE_NAME}:${IMAGE_TAG}
 DOCKER_FILE=docker/Dockerfile
-docker build -t ${DOCKER_IMAGE} -f ${DOCKER_FILE} .
+
+ARCH=`arch`
+OS_TYPE=`bash utils/detect_os_type.bash`
+
+if [ ${OS_TYPE} != "Mac" ]
+then
+	docker build -t ${DOCKER_IMAGE} -f ${DOCKER_FILE} .
+else
+	if [ $ARCH = "arm64" ]
+	then
+		docker build --platform linux/amd64 -t ${DOCKER_IMAGE} -f ${DOCKER_FILE} .
+
+	else
+		docker build -t ${DOCKER_IMAGE} -f ${DOCKER_FILE} .
+	fi
+fi
+
 
