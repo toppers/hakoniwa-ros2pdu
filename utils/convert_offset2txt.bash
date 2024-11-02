fe0ab6cd2dcd80b700598124e23533cff49779cf
Takashi Mori <kanetugu2015@gmail.com>
2024-11-02 11:01:11 +0900

    add util

---
+++ utils/convert_offset2txt.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 <target_dir>"
+    exit 1
+fi
+
+TARGET_DIR="$1"
+
+# .offsetファイルを再帰的に検索し、.textにリネーム
+find "$TARGET_DIR" -type f -name "*.offset" | while read file; do
+  new_file="${file%.offset}.txt"
+  mv "$file" "$new_file"
+  echo "Renamed: $file -> $new_file"
+done
+
+echo "All .offset files have been renamed to .txt"
