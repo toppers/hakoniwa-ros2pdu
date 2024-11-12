f37a1d7488d55794ef91d16d032cc1c42bd53b45
Takashi Mori <kanetugu2015@gmail.com>
2024-11-12 12:36:34 +0900

    add battery

---
+++ pdu/types/hako_msgs/pdu_cpptype_HakoBatteryStatus.hpp
@@ -0,0 +1,15 @@
+#ifndef _pdu_cpptype_hako_msgs_HakoBatteryStatus_HPP_
+#define _pdu_cpptype_hako_msgs_HakoBatteryStatus_HPP_
+
+#include "pdu_primitive_ctypes.h"
+#include <vector>
+#include <array>
+
+typedef struct {
+        Hako_float64 full_voltage;
+        Hako_float64 curr_voltage;
+        Hako_uint32 status;
+        Hako_uint32 cycles;
+} HakoCpp_HakoBatteryStatus;
+
+#endif /* _pdu_cpptype_hako_msgs_HakoBatteryStatus_HPP_ */
---
+++ pdu/types/hako_msgs/pdu_ctype_HakoBatteryStatus.h
@@ -0,0 +1,13 @@
+#ifndef _pdu_ctype_hako_msgs_HakoBatteryStatus_H_
+#define _pdu_ctype_hako_msgs_HakoBatteryStatus_H_
+
+#include "pdu_primitive_ctypes.h"
+
+typedef struct {
+        Hako_float64 full_voltage;
+        Hako_float64 curr_voltage;
+        Hako_uint32 status;
+        Hako_uint32 cycles;
+} Hako_HakoBatteryStatus;
+
+#endif /* _pdu_ctype_hako_msgs_HakoBatteryStatus_H_ */
