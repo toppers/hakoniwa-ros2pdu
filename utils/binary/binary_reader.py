40445374a70e7fb42475c8b68bec4299b6cfd7f1
Takashi Mori <kanetugu2015@gmail.com>
2025-07-26 11:05:05 +0900

    updated for latest

--- utils/binary/binary_reader.py
+++ utils/binary/binary_reader.py
@@ -2,68 +2,81 @@
 # -*- coding: utf-8 -*-
 import json
 import sys
-
-import binary_io
-import offset_parser
 import base64
 
-if len(sys.argv) != 4:
-	print("Usage: binary_reader.py <offset_path> <typename> <binary_file> ")
-	sys.exit()
+from . import binary_io
+from . import offset_parser
 
-offset_path=sys.argv[1] 
-typename=sys.argv[2]
-binary_filepath=sys.argv[3]
+def decode_base64(data):
+    return base64.b64decode(data)
 
-def binary_read_recursive(json_data, base_off, typename):
-    filepath = binary_io.find_filepath(offset_path +"/*/", typename + ".offset")
-    lines = offset_parser.parse_offset(filepath)
+def binary_read(offmap, typename, binary_data):
+    json_data = {}
+    meta_parser = binary_io.PduMetaDataParser()
+    meta = meta_parser.load_pdu_meta(binary_data)
+    if meta is None:
+        meta = binary_io.PduMetaData()
+        meta.set_empty()
+        #print("binary_data: size", len(binary_data))
+        #print("meta.to_bytes(): size", len(meta.to_bytes()))
+        binary_io.writeBinary(binary_data, 0, meta.to_bytes())
+    binary_read_recursive(meta, offmap, binary_data, json_data, binary_io.PduMetaData.PDU_META_DATA_SIZE, typename)
+    return json_data
+
+def binary_read_recursive(meta: binary_io.PduMetaData, offmap, binary_data, json_data, base_off, typename):
+    #lines = offmap[typename]
+    lines = offmap.get(typename)
     for line in lines:
         off = offset_parser.member_off(line) + base_off
         type = offset_parser.member_type(line)
         name = offset_parser.member_name(line)
         size = offset_parser.member_size(line)
+        #print(f"binary_read_recursive: {name} off={off} size={size} type={type}")
         if (offset_parser.is_primitive(line)):
             if (offset_parser.is_single(line)):
-                bin = binary_io.readBinary(binary_filepath, off, size)
+                bin = binary_io.readBinary(binary_data, off, size)
+                #print(f"binary_read_recursive: {name} off={off} size={size} type={type} bin={bin}")
                 value = binary_io.binTovalue(type, bin)
                 json_data[name] = value
-            else:
-                encode_type = "text"
-                array_size = offset_parser.array_size(line)
-                one_elm_size = int(size / array_size)
-                if (array_size > 100):
-                    encode_type = "base64"
-                    bin = binary_io.readBinary(binary_filepath, off, size)
-                    array_value = base64.b64encode(bin).decode('utf-8')
-                else:
-                    i = 0
-                    array_value = []
-                    while i < array_size:
-                        bin = binary_io.readBinary(binary_filepath, off + (i * one_elm_size), one_elm_size)
-                        value = binary_io.binTovalue(type, bin)
-                        array_value.append(value)
-                        i = i + 1
-                json_data[name + '_encode_type'] = encode_type
-                json_data[name] = array_value
+            elif (offset_parser.is_array(line)):
+                array_value = binary_io.readBinary(binary_data, off, size)
+                json_data[name + '__raw' ] = array_value
+                json_data[name] = binary_io.binToArrayValues(type, array_value)
+            else: #varray
+                array_size = binary_io.binTovalue("int32", binary_io.readBinary(binary_data, off, 4))
+                offset_from_heap = binary_io.binTovalue("int32", binary_io.readBinary(binary_data, off + 4, 4))
+                one_elm_size = size
+                #print(f"binary_read_recursive: {name} off={off} size={size} type={type} array_size={array_size} offset_from_heap={offset_from_heap}")
+                array_value = binary_io.readBinary(binary_data, meta.heap_off + offset_from_heap, one_elm_size * array_size)
+                json_data[name + '__raw' ] = array_value
+                json_data[name] = binary_io.binToArrayValues(type, array_value)
         else:
             if (offset_parser.is_single(line)):
                 tmp_json_data = {}
-                binary_read_recursive(tmp_json_data, off, type)
+                binary_read_recursive(meta, offmap, binary_data, tmp_json_data, off, type)
                 json_data[name] = tmp_json_data
-            else:
+            elif (offset_parser.is_array(line)):
                 i = 0
                 array_size = offset_parser.array_size(line)
                 one_elm_size = int(size / array_size)
                 array_value = []
                 while i < array_size:
                     tmp_json_data = {}
-                    binary_read_recursive(tmp_json_data, off + (i * one_elm_size), type)
+                    binary_read_recursive(meta, offmap, binary_data, tmp_json_data, off + (i * one_elm_size), type)
+                    array_value.append(tmp_json_data)
+                    i = i + 1
+                json_data[name] = array_value
+            else: #varray
+                array_size = binary_io.binTovalue("int32", binary_io.readBinary(binary_data, off, 4))
+                offset_from_heap = binary_io.binTovalue("int32", binary_io.readBinary(binary_data, off + 4, 4))
+                one_elm_size = size
+                i = 0
+                array_value = []
+                while i < array_size:
+                    tmp_json_data = {}
+                    binary_read_recursive(meta, offmap, binary_data, tmp_json_data, meta.heap_off + offset_from_heap + (i * one_elm_size), type)
                     array_value.append(tmp_json_data)
                     i = i + 1
                 json_data[name] = array_value
 
-json_data = {}
-binary_read_recursive(json_data, 0, typename)
 
-print(json.dumps(json_data))
--- utils/binary/binary_writer.py
+++ utils/binary/binary_writer.py
@@ -3,52 +3,130 @@
 import json
 import sys
 
-import binary_io
-import offset_parser
+from . import binary_io
+from . import offset_parser
 
-if len(sys.argv) != 5:
-	print("Usage: binary_writer.py <offset_path> <typename> <binary_def_json> <outfile>")
-	sys.exit()
+class DynamicAllocator:
+    def __init__(self, is_heap: bool):
+        self.data = bytearray()
+        self.offset_map = {}
+        self.is_heap = is_heap
 
-offset_path=sys.argv[1] 
-typename=sys.argv[2]
-binary_def_json=sys.argv[3]
-outfile=sys.argv[4]
+    def add(self, bytes_data, expected_offset=None, key=None):
+        current_size = len(self.data)
+        #print(f"is_heap: {self.is_heap} current_size: {current_size} expected_offset: {expected_offset} len(bytes_data): {len(bytes_data)}")
+        if (current_size < expected_offset):
+            padding = bytearray(expected_offset - current_size)
+            self.data.extend(padding)
+        offset = len(self.data)
+        self.data.extend(bytes_data)
+        #print(f"add: {bytes_data} offset: {offset} len(self.data): {len(self.data)}")
+        if key:
+            self.offset_map[key] = offset
+        
+        return offset
 
-json_open = open(binary_def_json, 'r')
-base_json_data = json.load(json_open)
+    def to_array(self):
+        return self.data
 
-def binary_write_recursive(json_data, base_off, typename):
-    filepath = binary_io.find_filepath(offset_path +"/*/", typename + ".offset")
-    lines = offset_parser.parse_offset(filepath)
-    for v in json_data['fields']:
-        line = offset_parser.select_by_name(lines, v['name'])
-        off = offset_parser.member_off(line) + base_off
+    def size(self):
+        return len(self.data)
+
+    def get_offset(self, key):
+        return self.offset_map.get(key, None)
+
+class BinaryWriterContainer:
+    def __init__(self):
+        self.heap_allocator = DynamicAllocator(True)
+        self.meta = binary_io.PduMetaData()
+        self.meta.set_empty()
+
+def binary_write(offmap, binary_data, json_data, typename):
+    base_allocator = DynamicAllocator(False)
+    bw_container = BinaryWriterContainer()
+    binary_write_recursive(0, bw_container, offmap, base_allocator, json_data, typename)
+
+    # メタデータの設定
+    total_size = base_allocator.size() + bw_container.heap_allocator.size() + binary_io.PduMetaData.PDU_META_DATA_SIZE
+    bw_container.meta.total_size = total_size
+    bw_container.meta.heap_off = binary_io.PduMetaData.PDU_META_DATA_SIZE + base_allocator.size()
+
+    # binary_data のサイズを total_size に調整
+    if len(binary_data) < total_size:
+        binary_data.extend(bytearray(total_size - len(binary_data)))
+    elif len(binary_data) > total_size:
+        del binary_data[total_size:]
+
+    # メタデータをバッファにコピー
+    binary_io.writeBinary(binary_data, 0, bw_container.meta.to_bytes())
+
+    # 基本データをバッファにコピー
+    binary_io.writeBinary(binary_data, bw_container.meta.base_off, base_allocator.to_array())
+
+    # ヒープデータをバッファにコピー
+    binary_io.writeBinary(binary_data, bw_container.meta.heap_off, bw_container.heap_allocator.to_array())
+
+def get_binary(type, bin, elm_size):
+    if type == "string":
+        buffer = bytearray(elm_size)
+        buffer[:len(bin)] = bin
+        return buffer
+    else:
+        return bin
+
+def binary_write_recursive(parent_off: int, bw_container: BinaryWriterContainer, offmap, allocator, json_data, typename):
+    lines = offmap.get(typename)
+    for key in json_data:
+        line = offset_parser.select_by_name(lines, key)
+        if line is None:
+            continue
         type = offset_parser.member_type(line)
-        if (offset_parser.is_primitive(line)):
-            if (offset_parser.is_single(line)):
-                bin = binary_io.typeTobin(type, v['value'])
-                binary_io.writeBinary(outfile, off, bin)
-            else:
-                i = 0
+        off = offset_parser.member_off(line)
+        #print(f"key: {key} type: {type} off: {off} line: {line}")
+        if offset_parser.is_primitive(line):
+            if offset_parser.is_single(line):
+                bin = binary_io.typeTobin(type, json_data[key])
+                bin = get_binary(type, bin, offset_parser.member_size(line))
+                # print(f"{type} {key} = {json_data[key]} : bin: {bin} size: {offset_parser.member_size(line)} bin_size: {len(bin)}")
+                allocator.add(bin, expected_offset=parent_off + off)
+            elif offset_parser.is_array(line):
                 elm_size = offset_parser.member_size(line)
                 array_size = offset_parser.array_size(line)
                 one_elm_size = int(elm_size / array_size)
-                for elm in v['value']:
-                    bin = binary_io.typeTobin(type, elm)
-                    binary_io.writeBinary(outfile, off + (i * one_elm_size), bin)
-                    i = i + 1
+                #for i, elm in enumerate(json_data[key]):
+                #    bin = binary_io.typeTobin(type, elm)
+                #    bin = get_binary(type, bin, one_elm_size)
+                #    allocator.add(bin, expected_offset=(parent_off + off + i * one_elm_size))
+                binary = binary_io.typeTobin_array(type, json_data[key], one_elm_size)
+                allocator.add(binary, expected_offset=(parent_off + off))
+            else:  # varray
+                offset_from_heap = bw_container.heap_allocator.size()
+                array_size = len(json_data[key])
+                #print(f"varray: {key} array_size: {array_size} offset_from_heap: {offset_from_heap}")
+                #for i, elm in enumerate(json_data[key]):
+                #    bin = binary_io.typeTobin(type, elm)
+                #    bin = get_binary(type, bin, offset_parser.member_size(line))
+                #    bw_container.heap_allocator.add(bin, expected_offset=0)
+                binary = binary_io.typeTobin_array(type, json_data[key], offset_parser.member_size(line))
+                bw_container.heap_allocator.add(binary, expected_offset=0)
+                a_b = array_size.to_bytes(4, byteorder='little')
+                o_b = offset_from_heap.to_bytes(4, byteorder='little')
+                allocator.add(a_b + o_b, expected_offset=parent_off + off)
         else:
-            if (offset_parser.is_single(line)):
-                binary_write_recursive(v['value'], off, type)
-            else:
-                i = 0
-                elm_size = offset_parser.member_size(line)
-                array_size = offset_parser.array_size(line)
-                one_elm_size = int(elm_size / array_size)
-                for elm in v['value']:
-                    binary_write_recursive(elm, off + (i * one_elm_size), type)
-                    i = i + 1
-            
+            if offset_parser.is_single(line):
+                binary_write_recursive(parent_off + off, bw_container, offmap, allocator, json_data[key], type)
+            elif offset_parser.is_array(line):
+                for i, elm in enumerate(json_data[key]):
+                    elm_size = offset_parser.member_size(line)
+                    array_size = offset_parser.array_size(line)
+                    one_elm_size = int(elm_size / array_size)
+                    binary_write_recursive((parent_off + off + i * one_elm_size), bw_container, offmap, allocator, elm, type)
+            else:  # varray
+                offset_from_heap = bw_container.heap_allocator.size()
+                array_size = len(json_data[key])
+                for i, elm in enumerate(json_data[key]):
+                    binary_write_recursive(0, bw_container, offmap, bw_container.heap_allocator, elm, type)
+                a_b = array_size.to_bytes(4, byteorder='little')
+                o_b = offset_from_heap.to_bytes(4, byteorder='little')
+                allocator.add(a_b + o_b, expected_offset=parent_off + off)
 
-binary_write_recursive(base_json_data, 0, typename)
